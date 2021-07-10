6f6bbc15de12c2f5595c0f6a409c902576bb65b2
michal-hynek <hynek.michal@gmail.com>
2021-07-10 14:09:05 -0700

    Made port configurable

--- serve.js
+++ serve.js
@@ -1,10 +1,14 @@
 var static = require('node-static');
+var dotenv = require('dotenv');
 var file = new static.Server('./');
+var defaultPort = 4000;
+
+dotenv.config();
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(4200);
+}).listen(process.env.PORT || defaultPort);
 
-console.log('Serving on http://localhost:4200/');
+console.log(`Serving on http://localhost:${process.env.PORT || defaultPort}/`);
